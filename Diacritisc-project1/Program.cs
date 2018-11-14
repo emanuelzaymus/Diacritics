d1664e1105becb6d41ff9fde5818e4f9f7f2550d
emanuelzaymus <emanuel.zaymus@gmail.com>
2018-11-14 08:39:42 +0100

    init

---
+++ Diacritisc-project1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diacritisc_project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
